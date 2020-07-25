9c3822e9a28b15202cfe1aeae1a6e070c30e268a
nick <njrusmc@Gmail.com>
2020-07-25 22:36:23 +0000

    pxgrid syncro

--- m2/send_radius.py
+++ m2/send_radius.py
@@ -3,7 +3,7 @@
 """
 Author: Nick Russo
 Purpose: Send bogus RADIUS packets to the DevNet ISE sandbox to simulate
-RADIUS failrues to test STOMP subscription telemetry.
+RADIUS failures to test STOMP subscription telemetry.
 """
 
 from radius import authenticate, NoResponse
@@ -11,7 +11,7 @@ from radius import authenticate, NoResponse
 if __name__ == "__main__":
     try:
         # Send some RADIUS packets to ISE with bogus credentials
-        authenticate("wrong_secret", "user", "pass", "10.10.20.70", port=1812)
+        authenticate("wrong_secret", "user", "pass", "ise24.abc.inc", port=1812)
     except NoResponse:
         # Exception should be raised, so do nothing
         pass
